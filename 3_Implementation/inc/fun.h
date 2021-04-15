f244268507322eb8f52bea03f59e609f112f5881
Akshay Mishra <70035586+akshay300712@users.noreply.github.com>
2021-04-15 13:43:35 +0530

    Create fun.h

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,16 @@
+/**
+ * @file fun.h
+ * @author Akshay Mishra
+ * @brief the header files which contains the prototype.
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __FUN_H__
+#define __FUN_H__
+
+void printMatrix();
+
+#endif /* __FUN_H__ */
